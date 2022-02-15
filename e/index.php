910f16e8fe7f2f4a0d3056c80ad9e53cd0a4d87b
Sara-Barbi <barbisara02@gmail.com>
2022-02-15 16:53:51 +0100

    array

---
+++ e/index.php
@@ -0,0 +1,52 @@
+<!--
+    Creare un array contenente qualche alunno di un’ipotetica
+    classe. Ogni alunno avrà Nome,ù
+    Cognome e un array contenente i suoi voti scolastici.
+    Stampare Nome, Cognome e la media dei voti di ogni alunno.
+-->
+
+<?php
+$alumnArr = [
+    [
+        "nome"=>"sara",
+        "cognome"=>"barbi",
+        [
+            "matematica"=>"6",
+            "italiano"=>"8",
+            "inglese"=>"7",
+            "informatica"=>"8"
+        ]
+    ],
+    [
+        "nome"=>"luigi",
+        "cognome"=>"mettuzzo",
+        [
+            "matematica"=>"6",
+            "italiano"=>"7",
+            "inglese"=>"6",
+            "informatica"=>"5"
+        ]
+    ],
+    [
+        "nome"=>"gaia",
+        "cognome"=>"mala",
+        [
+            "matematica"=>"8",
+            "italiano"=>"6",
+            "inglese"=>"7",
+            "informatica"=>"6"
+        ]
+    ],
+    [
+        "nome"=>"alessia",
+        "cognome"=>"cini",
+        [
+            "matematica"=>"7",
+            "italiano"=>"5",
+            "inglese"=>"6",
+            "informatica"=>"5"
+        ]
+    ],
+]
+
+?>
